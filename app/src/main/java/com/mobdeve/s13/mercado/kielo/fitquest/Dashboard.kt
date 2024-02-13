bee118172b126c8969b1c3dba558e6eac7f2a4e2
Kielo Bash Mercado <114970278+kieloBash@users.noreply.github.com>
2024-02-13 11:56:22 +0800

    made functional bottom navigationbar for fragments

--- app/src/main/java/com/mobdeve/s13/mercado/kielo/fitquest/Dashboard.kt
+++ app/src/main/java/com/mobdeve/s13/mercado/kielo/fitquest/Dashboard.kt
@@ -2,10 +2,33 @@ package com.mobdeve.s13.mercado.kielo.fitquest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        replaceFragment(Home())
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.navigation_home -> replaceFragment(Home())
+                R.id.navigation_calendar -> replaceFragment(Calendar())
+                R.id.navigation_workout -> replaceFragment(Workouts())
+
+                else -> {}
+
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame,fragment)
+        fragmentTransaction.commit()
     }
 }
